38da44acd7392d1c321986e0d401ecabd6a67f13
ice1000 <ice1000kotlin@foxmail.com>
2016-09-03 23:47:24 +0800

    commit via a script

---
+++ demo/Demo19.kt
@@ -0,0 +1,32 @@
+import org.frice.game.utils.data.FileUtils
+import org.frice.game.utils.kotlin.forceRun
+import org.frice.game.utils.message.log.FLog
+import org.frice.game.utils.web.*
+import java.util.*
+
+/**
+ * test: http://lofi.e-hentai.org/g/813858/63f5aba277/
+ * test: http://www.yaoqmh.net/shaonvmanhua/5122.html
+ * Created by ice1000 on 2016/9/3.
+
+ * @author ice1000
+ */
+class EDemo {
+	fun process() {
+		val scanner = Scanner(System.`in`)
+		var count = 0
+		while (scanner.hasNext()) forceRun {
+			val images = HTMLUtils.findTag(WebUtils.readText(scanner.nextLine()), "img")
+			images.forEachIndexed { index, i ->
+				FLog.i("processing $i.... please wait.")
+				val from = i.indexOf('\"') + 1
+				val link = i.substring(from, i.indexOf('\"', from))
+				FLog.i("url is $link")
+				forceRun { FileUtils.image2File(WebUtils.readImage(link), "$count.png") }
+				count++
+			}
+		}
+	}
+}
+
+fun main(args: Array<String>) = EDemo().process()
