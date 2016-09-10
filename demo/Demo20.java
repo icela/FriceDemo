d94d755db692eb8875c7f7143705e0b342076781
master <951394653@qq.com>
2016-09-10 17:54:12 +0800

    add demo

---
+++ demo/Demo20.java
@@ -0,0 +1,28 @@
+import org.frice.game.anim.move.AccelerateMove;
+import org.frice.game.obj.sub.ShapeObject;
+import org.frice.game.resource.graphics.ColorResource;
+import org.frice.game.resource.image.ImageResource;
+import org.frice.game.utils.graphics.shape.FCircle;
+import org.frice.game.utils.graphics.utils.ColorUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ice1000 on 2016/8/29.
+ *
+ * @author ice1000
+ */
+public class Demo20 extends Game {
+	@Override
+	protected void onRefresh() {
+		addObject(new ShapeObject(ColorResource.BLUE, new FCircle(30), getMousePosition().x, getMousePosition().y) {{
+			getAnims().add(AccelerateMove.getGravity());
+		}});
+	}
+
+	public static void main(String[] args) {
+		new Demo20();
+	}
+}
