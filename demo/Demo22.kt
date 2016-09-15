52406ad27e97c3ae34cb5bb8662eef5e39681370
master <951394653@qq.com>
2016-09-15 09:52:32 +0800

    update anim

---
+++ demo/Demo22.kt
@@ -0,0 +1,80 @@
+package org.frice.game
+
+import org.frice.game.obj.sub.ShapeObject
+import org.frice.game.resource.graphics.ColorResource
+import org.frice.game.utils.graphics.shape.FCircle
+import org.frice.game.utils.graphics.shape.FRectangle
+import org.frice.game.utils.time.FTimer
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.util.*
+import java.util.concurrent.LinkedBlockingQueue
+
+class Demo22 : Game() {
+
+	private var direction = KeyEvent.VK_LEFT
+	private var xx = 30
+	private var yy = 20
+	private var fx = 10
+	private var fy = 10
+	private var timer = FTimer(100)
+	private var isIncreasing = true
+	lateinit private var body: Queue<ShapeObject>
+	lateinit private var food: ShapeObject
+
+	override fun onInit() {
+		super.onInit()
+		setLocationRelativeTo(null)
+		setSize(600, 400)
+		title = "Snake game demo"
+		addKeyListener(object : KeyListener {
+			override fun keyTyped(e: KeyEvent) = Unit
+			override fun keyPressed(e: KeyEvent) {
+				direction = e.keyCode
+			}
+
+			override fun keyReleased(e: KeyEvent) = Unit
+		})
+		body = LinkedBlockingQueue<ShapeObject>()
+		food = generateFood(10, 10)
+		addObject(food)
+		addObject(generateBody(30, 20).apply { body.add(this) })
+		autoGC = false
+		isResizable = false
+	}
+
+	override fun onRefresh() {
+		super.onRefresh()
+		if (timer.ended()) {
+			val obj: ShapeObject
+			when (direction) {
+				KeyEvent.VK_LEFT -> obj = generateBody(--xx, yy)
+				KeyEvent.VK_RIGHT -> obj = generateBody(++xx, yy)
+				KeyEvent.VK_UP -> obj = generateBody(xx, --yy)
+				else -> obj = generateBody(xx, ++yy)
+			}
+			xx = moved(xx, true)
+			yy = moved(yy, false)
+			if (xx == fx && yy == fy) {
+				removeObject(food)
+				isIncreasing = true
+				fx = random.nextInt(this.width / 10 - 1)
+				fy = random.nextInt(this.height / 10 - 1)
+				food = generateFood(fx, fy)
+				addObject(food)
+			}
+			body.add(obj)
+			addObject(obj)
+			if (!isIncreasing) removeObject(body.poll())
+			isIncreasing = false
+		}
+	}
+
+	private fun generateBody(x: Int, y: Int) = ShapeObject(ColorResource.吾王蓝, FRectangle(10, 10), (x * 10).toDouble(), (y * 10).toDouble())
+	private fun generateFood(x: Int, y: Int) = ShapeObject(ColorResource.如果奇迹有颜色那么一定是橙色, FCircle(5.0), (x * 10).toDouble(), (y * 10).toDouble())
+	private fun moved(i: Int, isX: Boolean) = if (isX) (i + this.width / 10 - 2) % (this.width / 10 - 2) else (i + (this.height / 10 - 4)) % (this.height / 10 - 4)
+
+	companion object {
+		@JvmStatic fun main(args: Array<String>) = Demo22()
+	}
+}
