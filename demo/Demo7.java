baef1d1a840e877462b50d8c9528601da153428d
Tesla Ice Zhang <ice1000@kotliner.cn>
2017-08-12 00:10:16 +0800

    More beautiful

--- demo/Demo7.java
+++ demo/Demo7.java
@@ -1,71 +1,59 @@
-import kotlin.Pair;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
 import org.frice.game.Game;
-import org.frice.game.anim.move.AccelerateMove;
-import org.frice.game.anim.move.SimpleMove;
-import org.frice.game.obj.FObject;
+import org.frice.game.anim.move.*;
 import org.frice.game.obj.sub.ShapeObject;
+import org.frice.game.platform.adapter.JvmImage;
 import org.frice.game.resource.graphics.ColorResource;
-import org.frice.game.utils.graphics.shape.FCircle;
-import org.frice.game.utils.graphics.shape.FRectangle;
+import org.frice.game.utils.data.FileUtils;
+import org.frice.game.utils.graphics.shape.*;
 import org.frice.game.utils.message.FDialog;
 import org.frice.game.utils.time.FTimer;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
-
 public class Demo7 extends Game {
 	public static void main(String[] args) {
-		new Demo7();
+		launch(Demo7.class);
 	}
 
-	private Random random = new Random();
 	private FTimer timer = new FTimer(3000);
 	private ShapeObject object;
-	private FObject.OnCollideEvent gameOver;
+	private Function0<Unit> gameOver;
 
 	@Override
-	protected void onInit() {
+	public void onInit() {
 		setSize(500, 800);
 		setTitle("Flappy bird demo by ice1000");
-		object = new ShapeObject(ColorResource.宝强绿, new FCircle(20.0), 50.0, 200.0);
-		object.getAnims().add(AccelerateMove.getGravity());
+		object = new ShapeObject(ColorResource.宝强绿, new FCircle(20), 50, 200);
+		object.addAnim(AccelerateMove.getGravity());
 		addObject(object);
 		gameOver = () -> {
-			new Thread(() -> {
-				try {
-					ImageIO.write(getScreenCut().getImage(), "png", new File("截屏.png"));
-				} catch (IOException ignored) {
-					System.out.println("车祸现场");
-				}
-			}).start();
+			new Thread(() -> FileUtils.image2File((JvmImage) getScreenCut().getImage(), "截屏.png")).start();
 			new FDialog(this).show("Game Over");
 			System.exit(0);
+			return Unit.INSTANCE;
 		};
-		listenKeyPressed(e -> {
-			object.getAnims().clear();
-			object.getAnims().add(AccelerateMove.getGravity());
-			object.getAnims().add(new SimpleMove(0, -400));
-		});
+		addKeyListener(null, e -> {
+			object.stopAnims();
+			object.addAnim(AccelerateMove.getGravity());
+			object.addAnim(new SimpleMove(0, -400));
+			return Unit.INSTANCE;
+		}, null);
 	}
 
 	@Override
-	protected void onRefresh() {
-		if (object.getY() > getHeight() + 20) gameOver.handle();
-		if (timer.ended()) addObjects(getObj());
+	public void onRefresh() {
+		if (object.getY() > getHeight() + 20) gameOver.invoke();
+		if (timer.ended()) addObject(getObj());
 	}
 
 	private ShapeObject[] getObj() {
-		int height = random.nextInt(400);
-		return new ShapeObject[]{new ShapeObject(ColorResource.教主黄,
-				new FRectangle(50, height), 550.0, 0.0) {{
-			getAnims().add(new SimpleMove(-150, 0));
-			getTargets().add(new Pair<>(object, gameOver));
-		}}, new ShapeObject(ColorResource.教主黄,
-				new FRectangle(50, getHeight() - height - 400), 550.0, height + 400.0) {{
-			getAnims().add(new SimpleMove(-150, 0));
-			getTargets().add(new Pair<>(object, gameOver));
+		int height = getRandom().nextInt(400);
+		return new ShapeObject[]{new ShapeObject(ColorResource.教主黄, new FRectangle(50, height), 550, 0) {{
+			addAnim(new SimpleMove(-150, 0));
+			addCollider(object, gameOver);
+		}}, new ShapeObject(ColorResource.教主黄, new FRectangle(50, getHeight() - height - 400), 550, height + 400) {{
+			addAnim(new SimpleMove(-150, 0));
+			addCollider(object, gameOver);
 		}}};
 	}
 }
