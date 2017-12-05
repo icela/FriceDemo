99454ded514fc1c0670be8f43423bc3759685425
ice1000 <ice1000kotlin@foxmail.com>
2017-12-05 12:07:44 +0800

    666

--- 1.7.0/Demo7QuadTree.java
+++ 1.7.0/Demo7QuadTree.java
@@ -28,61 +28,50 @@ public class Demo7QuadTree extends Game {
 
 	private Random random = new Random();
 	private FTimer timer = new FTimer(3000);
-	private ShapeObject object;
+	private ShapeObject bird;
 	private SideEffect gameOver;
 
 	@Override
 	public void onInit() {
 		setSize(500, 800);
 		setTitle("Flappy bird demo by ice1000");
-		object = new ShapeObject(ColorResource.宝强绿, new FCircle(20.0), 50.0, 200.0);
-		object.getAnims().add(AccelerateMove.getGravity());
-		addObject(object);
+		bird = new ShapeObject(ColorResource.宝强绿, new FCircle(20.0), 50.0, 200.0);
+		bird.getAnims().add(AccelerateMove.getGravity());
+		addObject(bird);
 		gameOver = () -> {
 			dialogShow("Game Over");
 			System.exit(0);
 		};
 		addKeyListener(null, e -> {
-			object.stopAnims();
-			object.addAnim(AccelerateMove.getGravity());
-			object.addAnim(new SimpleMove(0, -400));
+			bird.stopAnims();
+			bird.addAnim(AccelerateMove.getGravity());
+			bird.addAnim(new SimpleMove(0, -400));
 		}, null);
 	}
 
 	@Override
 	public void onRefresh() {
 		QuadTree quadTree = new QuadTree(0, new FQuad(0, 0, 500, 800));
-
-		if (object.getY() > getHeight() + 20) gameOver.invoke();
-
-		ShapeObject[] newObjects;
-
+		if (bird.getY() > getHeight() + 20) gameOver.invoke();
 		if (timer.ended()) {
-			newObjects = getObj(random.nextInt(400));
+			ShapeObject[] newObjects = getObj(random.nextInt(400));
 			addObject(newObjects);
 			Collections.addAll(objects(), newObjects);
 		}
 
 		quadTree.clear();
-		for (Object abstractObject : objects())
-			if (abstractObject instanceof PhysicalObject) quadTree.insert((PhysicalObject) abstractObject);
-
+		objects().stream().filter(obj -> obj instanceof PhysicalObject).map(obj -> (PhysicalObject) obj).forEachOrdered(quadTree::insert);
 		ArrayList<List<PhysicalObject>> returnObjects = new ArrayList<>();
-
-		returnObjects.clear();
-		quadTree.retrieve(returnObjects, object);
-
+		quadTree.retrieve(returnObjects, bird);
 		FLog.e(" size " + returnObjects.size());
 
-		for (List<PhysicalObject> returnObject : returnObjects) {
-			if (returnObject.contains(object)) {
-				returnObject.remove(object);
-				for (PhysicalObject physicalObject : returnObject) {
-					((ShapeObject) physicalObject).setRes(ColorResource.基佬紫);
-					if (rectCollideRect(object, physicalObject)) gameOver.invoke();
-				}
-			}
-		}
+		returnObjects.stream().filter(objs -> objs.contains(bird)).forEachOrdered(rets -> {
+			rets.remove(bird);
+			rets.forEach(physicalObject -> {
+				((ShapeObject) physicalObject).setRes(ColorResource.基佬紫);
+				if (bird.collides(physicalObject)) gameOver.invoke();
+			});
+		});
 	}
 
 	@NotNull
@@ -90,21 +79,14 @@ public class Demo7QuadTree extends Game {
 		return getLayers()[0].getObjects();
 	}
 
-	private boolean rectCollideRect(PhysicalObject A, PhysicalObject B) {
-		return A.getX() + A.getWidth() >= B.getX()
-				&& B.getY() <= A.getY() + A.getHeight()
-				&& A.getX() <= B.getX() + B.getWidth()
-				&& A.getY() <= B.getY() + B.getHeight();
-	}
-
-
+	@NotNull
 	private ShapeObject[] getObj(int height) {
 		return new ShapeObject[]{new ShapeObject(ColorResource.教主黄, new FRectangle(50, height), 550, 0) {{
 			addAnim(new SimpleMove(-150, 0));
-			addCollider(object, gameOver);
+			addCollider(bird, gameOver);
 		}}, new ShapeObject(ColorResource.教主黄, new FRectangle(50, getHeight() - height - 400), 550, height + 400) {{
 			addAnim(new SimpleMove(-150, 0));
-			addCollider(object, gameOver);
+			addCollider(bird, gameOver);
 		}}};
 	}
 }
